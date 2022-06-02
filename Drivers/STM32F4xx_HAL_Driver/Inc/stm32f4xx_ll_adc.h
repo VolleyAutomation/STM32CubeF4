c0ce01f5823540e79ee147a7c84c8baa7074fa87
Mark Cutler <markjcutler@gmail.com>
2022-06-02 11:20:30 -0700

    testing

--- Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_adc.h
+++ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_adc.h
@@ -294,7 +294,7 @@ extern "C" {
   * @retval Pointer to register address
   */
 #define __ADC_PTR_REG_OFFSET(__REG__, __REG_OFFFSET__)                         \
- ((__IO uint32_t *)((uint32_t) ((uint32_t)(&(__REG__)) + ((__REG_OFFFSET__) << 2UL))))
+ ((__IO uint32_t *)((uint32_t) (reinterpret_cast<unsigned long>(&(__REG__)) + ((__REG_OFFFSET__) << 2UL))))
 
 /**
   * @}
@@ -1861,17 +1861,17 @@ typedef struct
 #if defined(ADC_MULTIMODE_SUPPORT)
 __STATIC_INLINE uint32_t LL_ADC_DMA_GetRegAddr(ADC_TypeDef *ADCx, uint32_t Register)
 {
-  uint32_t data_reg_addr = 0UL;
+  unsigned long data_reg_addr = 0UL;
   
   if (Register == LL_ADC_DMA_REG_REGULAR_DATA)
   {
     /* Retrieve address of register DR */
-    data_reg_addr = (uint32_t)&(ADCx->DR);
+    data_reg_addr = reinterpret_cast<unsigned long>(&(ADCx->DR));
   }
   else /* (Register == LL_ADC_DMA_REG_REGULAR_DATA_MULTI) */
   {
     /* Retrieve address of register CDR */
-    data_reg_addr = (uint32_t)&((__LL_ADC_COMMON_INSTANCE(ADCx))->CDR);
+    data_reg_addr = reinterpret_cast<unsigned long>(&((__LL_ADC_COMMON_INSTANCE(ADCx))->CDR));
   }
   
   return data_reg_addr;
